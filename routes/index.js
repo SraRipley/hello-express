1e28837b7874a763ba71101d9717d8dae6701231
afd <afd@alup5.academiapostal.es.local>
2020-01-29 17:42:39 +0100

    pagina de error

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const products = [
+    {nombre: "Hojalata", precio: 45, existencias: 10},
+    {nombre: "León", precio: 54, existencias: 1},
+    {nombre: "Totó", precio: 1, existencias: 0},
+    {nombre: "Espantapájaros", precio: 10000, existencias: 100},
+    {nombre: "Dorothy", precio: 10, existencias: 2},
+    {nombre: "Bruja malvada del Oeste", precio: 50, existencias: 5}
+  ];
+  res.render('index', { title: 'Oz',products });
 });
 
 module.exports = router;
