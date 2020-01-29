dac6130772cb85944324735d6759a5b59a2a2c89
afd <afd@alup5.academiapostal.es.local>
2020-01-29 18:30:27 +0100

    imagenes

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const products = [
-    {nombre: "Hojalata", precio: 45, existencias: 10},
-    {nombre: "León", precio: 54, existencias: 1},
-    {nombre: "Totó", precio: 1, existencias: 0},
-    {nombre: "Espantapájaros", precio: 10000, existencias: 100},
-    {nombre: "Dorothy", precio: 10, existencias: 2},
-    {nombre: "Bruja malvada del Oeste", precio: 50, existencias: 5}
+    {nombre: "Hojalata", precio: 45, existencias: 10, imagen: "hojalata.jpg"},
+    {nombre: "León", precio: 54, existencias: 1, imagen:"leon.jpg"},
+    {nombre: "Totó", precio: 1, existencias: 0, imagen:"toto.jpg"},
+    {nombre: "Espantapájaros", precio: 10000, existencias: 100, imagen:"espanta1.jpg"},
+    {nombre: "Dorothy", precio: 10, existencias: 2, imagen:"dorothy.jpg"},
+    {nombre: "Bruja malvada del Oeste", precio: 50, existencias: 5, imagen:"oeste1.jpg"}
   ];
   res.render('index', { title: 'Oz',products });
 });
