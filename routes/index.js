511f2aa5420c162636b7c185c7b5077ac1244c2d
afd <afd@alup5.academiapostal.es.local>
2020-01-30 18:15:55 +0100

    boton formulario compra

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,11 @@ router.get('/products/:ref', function(req, res, next) {
   const product = products.find(function(p) {
     return p.ref == ref;
   });
+  if (product){
   res.render("producto",{product});
+  }else{
+    res.redirect("/error");
+  }
 });
 
 module.exports=router;
