2398c4ef83a3c7b6a063b31a7aacd1bde1edf1e2
afd <afd@alup5.academiapostal.es.local>
2020-01-31 17:56:27 +0100

    login

---
+++ models/users.js
@@ -0,0 +1,6 @@
+module.exports = [
+    {username:"jairo", password:"123456"},
+    {username:"veronica", password:"4321"},
+    {username:"elena", password:"1234567"},
+    {username:"pablo", password:"123"}
+];
